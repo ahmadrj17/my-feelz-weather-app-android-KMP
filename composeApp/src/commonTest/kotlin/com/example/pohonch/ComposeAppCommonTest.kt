2ad960b19a92f2d51336974625355d9699583946
Nabil Ashraf <nabilashraf@chumchum.agency>
2025-12-11 23:49:13 +0500

    refactor the project

---
+++ composeApp/src/commonTest/kotlin/com/example/pohonch/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.example.pohonch
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
